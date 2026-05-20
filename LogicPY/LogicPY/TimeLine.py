e09cc3c8fb66bdb49d76f620ebeb623b3b7a1233
ThisIsACryForHelp <nv7033190@gmail.com>
2026-05-20 23:17:52 +0300

    started with the TimeLine.py

--- LogicPY/LogicPY/TimeLine.py
+++ LogicPY/LogicPY/TimeLine.py
@@ -1,3 +1,4 @@
+import pygame
 class TimeLine:
     """
     a specific timeline with a specific position 
@@ -7,12 +8,31 @@ class TimeLine:
         self.TimeLineID = TimeLineID
         self.ParentID = ParentID
         self.CreationTurn = CreationTurn
-        self.BoardToCopy = BoardToCopy
+        self.TesseractBoard = BoardToCopy
 
     @classmethod
-    def BuildTesseract(cls, TimeLineID, Position_String):
-        instance = cls(timeline_id)
-        Positions = Position_String.split("-")
-        for x in range(4):
-            for y in range(4):
-                for z in range(4):
+    def BuildTesseract(self, TimeLineID, screen, Position_Dict = None):
+        SQUARE_SIZE = 45
+        BOARD_MARGIN = SQUARE_SIZE * 4 + 20 
+        #each board is 4 by 4 squares, so the whole board is 180 pixels
+        #and add a 20 pixel margin 
+        self.TimeLineID = TimeLineID
+        self.TesseractBoard = Position_Dict
+        for w in range(4):
+            for x in range(4):
+                SquareCounter = 0
+                for y in range(4):
+                    for z in range(4):
+                        x_axis_pixel = y * SQUARE_SIZE
+                        y_axis_pixel = z * SQUARE_SIZE
+                        BoardSquare = pygame.Rect(x_axis_pixel, y_axis_pixel, SQUARE_SIZE, SQUARE_SIZE)
+                        color = (240, 217, 181) if (y + z) % 2 == 0 else (181, 136, 99)
+                        pygame.draw.rect(screen, color, BoardSquare)
+                        SquareCounter += 1
+
+
+                    
+
+
+
+                    
